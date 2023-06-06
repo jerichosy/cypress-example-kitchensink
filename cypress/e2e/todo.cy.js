d3b84ab0b0da8011bbc1a5fa49b6f02939c88e11
Matthew Jericho Sy <matthew_sy@dlsu.edu.ph>
2023-06-06 09:54:10 +0800

    can add new todo items

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -2,4 +2,15 @@ describe('todomvc app', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080/todo')
   })
+
+  it('can add new todo items', () => {
+    const newItem = 'Feed the cat'
+    cy.get('[data-test=new-todo]').type(`${newItem}{enter}`)
+
+    // assert
+    cy.get('.todo-list li')
+      .should('have.length', 3)
+      .last()
+      .should('have.text', newItem)
+  })
 })
